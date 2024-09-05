e73a699178a42aed8348e7e5d727912235df3575
harshdev2909 <harsh.dev2909@gmail.com>
2024-09-05 22:31:29 +0530

    user created

--- server/routes/useRoute.js
+++ server/routes/useRoute.js
@@ -3,5 +3,6 @@ const userCtrl = require('../controllers/userCtrl');
 const router = require('express').Router();
 
 router.post('/register',userCtrl.register)
+router.post('/refresh_token',userCtrl.register)
 
 module.exports =  router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser())
 
 const port = process.env.port || 5000;
 app.get("/",(req,res)=>{
@@ -15,16 +18,13 @@ app.listen(port,() =>{
 app.use('/user',require('./routes/useRoute'));
 
 //connect mongoDb
-const URL = process.env.MONGODB_URL;
+const URL = 'mongodb+srv://harshdev2909:harsh9560@cluster0.gkf5y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
-mongoose.connect(URL,{
-    useCreateIndex:true,
-    useFindAndModify:false,
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
+mongoose.connect(URL)
+.then(()=>{
     console.log("Database connected")
+    mongoose.set('useUnifiedTopology', true);
+    mongoose.set('useNewUrlParser', true);
 }).catch(err =>{
     console.log(err);
 })
-
