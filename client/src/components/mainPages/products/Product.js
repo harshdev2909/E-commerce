c61389507344dcd6c066bae5cc7c1a27dcfb45c6
harshdev2909 <harsh.dev2909@gmail.com>
2024-09-14 21:45:48 +0530

    changes done

--- client/src/components/mainPages/products/Product.js
+++ client/src/components/mainPages/products/Product.js
@@ -5,12 +5,13 @@ import ProductList from '../utils/productlist/ProductList'
 const Product = () => {
   const state =useContext(GlobalState)
   const [products] = state.productAPI.products
+
   console.log(state)
   return (
     <div className='products'>
         {
           products.map(product=> {
-            return <ProductList key={product._id} product={product}/>
+            return <ProductList key={product.id} product={product}/>
           })
         }
     </div>
--- client/src/components/mainPages/utils/detailProducts/DetailProduct.js
+++ client/src/components/mainPages/utils/detailProducts/DetailProduct.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { GlobalState } from '../../../../GlobalState'
 
 const DetailProduct = () => {
+    const params = useParams()
+    const state = useContext(GlobalState)
+    const [products] = state.productAPI.products
+
+    const[details,setDetails] =  useState([])
+
+    useEffect(()=>{
+        if(params){
+            products.forEach(product=> {
+                if(product._id === params.id){
+                    setDetails(product)
+                }
+            })
+        }
+    },[params,products])
+    console.log(details)
   return (
-    <div>DetailProduct</div>
+    <div className='detail'>
+        {/* <img src={details.images.url}/> */}
+
+    </div>
+
   )
 }
 
